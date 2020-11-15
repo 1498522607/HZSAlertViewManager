c0f52098e51f4592e80b3d669233c390d26e4b9e
hzsMac <huangzushu@ds.cn>
2020-11-15 23:54:06 +0800

    返回修饰符

--- HZSAlertViewManager/Classes/HZSAlertViewManager.h
+++ HZSAlertViewManager/Classes/HZSAlertViewManager.h
@@ -16,7 +16,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 //重要提醒！:自定义的弹窗不要添加再[UIApplication sharedApplication].keyWindow上, keyWindow是会变化的，因为系统的弹窗是创建一个window，成为keyWindow，当系统弹窗消失的时候，创建出来的window也会销毁，但是这时候获取的keyWindow有可能就是准备销毁这个.这样的自定义弹窗连同keywindow一起被销毁，导致信号一直是锁着的状态，无法弹出后面的弹窗
 //系统弹窗消失瞬间获取到的keyWindow就是系统弹窗那个keyWindow，自定义弹窗添加上去会随着keywindow销毁而销毁
-+ (nullable instancetype)shareManager;
++ (instancetype)shareManager;
 /**
  弹窗显示的代码写进BLOCK块内
  @param executeBlock <#executeBlock description#>
